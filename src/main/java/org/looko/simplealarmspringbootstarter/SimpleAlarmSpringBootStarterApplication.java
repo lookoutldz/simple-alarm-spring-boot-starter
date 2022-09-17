e8fdd1464cde16a92e3dbb2a574d2bbb0a15c3e5
looko <lookoutldz@outlook.com>
2022-09-17 10:37:23 +0800

    Initial commit

---
+++ src/main/java/org/looko/simplealarmspringbootstarter/SimpleAlarmSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package org.looko.simplealarmspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAlarmSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAlarmSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/looko/simplealarmspringbootstarter/SimpleAlarmSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.looko.simplealarmspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAlarmSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
